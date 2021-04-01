23755ae6901bbd5293aca74c9b62262c7da24ec6
JHSeo-git <qnfqnfqnf@gmail.com>
2021-04-01 17:29:33 +0900

    [업데이트] tui-editor syntax highlight

--- packages/pr-fe/src/components/Editor/Editor.tsx
+++ packages/pr-fe/src/components/Editor/Editor.tsx
@@ -1,43 +1,51 @@
-import { useRef } from 'react';
-import { css } from '@emotion/react';
-import { Editor as ToastEditor } from '@toast-ui/react-editor';
-import hljs from 'highlight.js';
-import codeSyntaxHighlightPlugin from '@toast-ui/editor-plugin-code-syntax-highlight';
-import { useEditorMarkdownState } from '@src/states/editorStates';
-
 import 'codemirror/lib/codemirror.css';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import 'highlight.js/styles/github.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { useRef } from 'react';
+import { Editor as ReactEditor } from '@toast-ui/react-editor';
+import ToastEditor from '@toast-ui/editor';
+import hljs from 'highlight.js';
+import { useEditorMarkdownState } from '@src/states/editorStates';
 
 export type EditorProps = {};
 
 function Editor(props: EditorProps) {
-  const editorRef = useRef<ToastEditor>(null);
+  const editorRef = useRef<ReactEditor>(null);
   const [, setEditorMarkdown] = useEditorMarkdownState();
+
   const onChange = () => {
     if (!editorRef.current) return;
     setEditorMarkdown(editorRef.current.getInstance().getMarkdown());
   };
+
+  const syntaxHighlightPlugIn = () => {
+    const languages = hljs.listLanguages();
+    languages.forEach((type) => {
+      const convertor = (codeText: string) =>
+        hljs.highlight(codeText, { language: type }).value;
+      const aliases = hljs.getLanguage(type)?.aliases || [];
+      const langTypes = [type, ...aliases];
+
+      langTypes.forEach((lang) => {
+        ToastEditor.codeBlockManager.setReplacer(lang, convertor);
+      });
+    });
+  };
+
   return (
-    <div css={wrapper}>
-      <ToastEditor
-        ref={editorRef}
-        previewStyle="vertical"
-        height="100%"
-        initialEditType="markdown"
-        hideModeSwitch={true}
-        plugins={[codeSyntaxHighlightPlugin.bind(hljs)]}
-        events={{
-          change: onChange,
-        }}
-      />
-    </div>
+    <ReactEditor
+      ref={editorRef}
+      initialEditType="markdown"
+      previewStyle="vertical"
+      height="100%"
+      hideModeSwitch={true}
+      events={{
+        change: onChange,
+      }}
+      plugins={[syntaxHighlightPlugIn]}
+    />
   );
 }
-
-const wrapper = css`
-  flex: 1;
-`;
 
 export default Editor;
